8b148c388bdd263d6f3cb2ed3a684c98bfb5f04f
tanvy <tanvyait@gmail.com>
2021-07-01 12:42:59 +0530

    added mute function and stoop video function

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io= require('socket.io')(server);
-
-const { ExpressPeerServer} = require('peer');
-const peerServer = ExpressPeerServer(server,{
-    debug: true
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
 });
-const { v4:uuidv4 } = require('uuid');
+const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-// directs us in room with particular ID
-app.get('/', (req, res)=>{
-    res.redirect(`/${uuidv4()}`);
-    
-})
-// way to room
-app.get('/:room', (req, res)=>{
-    res.render('room', { roomId: req.params.room})
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-// confirmation after joining room 
-io.on('connection', socket =>{
-    socket.on('join-room', (roomId, userId)=>{
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
-    
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
 
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
 
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, console.log(`The Server is Running on PORT : ${PORT}`));
+server.listen(process.env.PORT||3030)
